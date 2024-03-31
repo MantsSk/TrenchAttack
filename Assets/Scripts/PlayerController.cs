d74a5e17e13d0ccc76338ffa2d02d1724afeccef
MSkara <mantas.skara@gmail.com>
2024-03-31 18:30:06 +0300

    movement

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,7 +6,11 @@ public class PlayerController : MonoBehaviour
     public float moveSpeed;
     private CharacterController characterController;
     private Vector3 moveInput;
-    public Transform camTrans;
+    // public Transform camTrans;
+    public bool invertX = false;
+    public bool invertY = false;
+
+    public float mouseSenisitivity = 2.0f;
 
     void Start()
     {
@@ -16,15 +20,30 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-       moveInput.x = Input.GetAxis("Horizontal") * moveSpeed * Time.deltaTime;
-       moveInput.z = Input.GetAxis("Vertical") * moveSpeed * Time.deltaTime; 
+    //    moveInput.x = Input.GetAxis("Horizontal") * moveSpeed * Time.deltaTime;
+    //    moveInput.z = Input.GetAxis("Vertical") * moveSpeed * Time.deltaTime; 
+        Vector3 vertMove = transform.forward * Input.GetAxis("Vertical");
+        Vector3 horiMove = transform.right * Input.GetAxis("Horizontal");
 
-       characterController.Move(moveInput);
+        moveInput = vertMove + horiMove;
 
-       Vector2 mouseInput = new Vector2(Input.GetAxisRaw("Mouse X"), Input.GetAxisRaw("Mouse Y"));
+        moveInput = moveInput.normalized * moveSpeed;
 
-       transform.rotation = Quaternion.Euler(transform.rotation.eulerAngles.x, transform.rotation.eulerAngles.y + mouseInput.x, transform.rotation.eulerAngles.z);
+        characterController.Move(moveInput * Time.deltaTime);
 
-       camTrans.rotation = Quaternion.Euler(camTrans.rotation.eulerAngles + new Vector3(-mouseInput.y, 0f, 0f));
+        Vector2 mouseInput = new Vector2(Input.GetAxisRaw("Mouse X"), Input.GetAxisRaw("Mouse Y")) * mouseSenisitivity;
+
+        if(invertX)
+        {
+            mouseInput.x = -mouseInput.x;
+        }
+        if(invertY)
+        {
+            mouseInput.y = -mouseInput.y;
+        }
+
+        transform.rotation = Quaternion.Euler(transform.rotation.eulerAngles.x, transform.rotation.eulerAngles.y + mouseInput.x, transform.rotation.eulerAngles.z);
+
+        // camTrans.rotation = Quaternion.Euler(camTrans.rotation.eulerAngles + new Vector3(-mouseInput.y, 0f, 0f));
     }
 }
