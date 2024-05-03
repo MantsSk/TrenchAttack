4d466c9db96f21becc4fcdbdeea97364a48a744c
MSkara <mantas.skara@gmail.com>
2024-05-03 12:43:02 +0300

    shooting mechanics

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -7,13 +7,15 @@ public class Enemy : MonoBehaviour
 {
     public float moveSpeed = 6;
     private Rigidbody rb;
+    private Animator animator;
     private Vector3 targetPoint, startPoint;
     private NavMeshAgent agent;
     int currentHealth = 100;
     private bool chasing = false;
+    private bool isShooting = false;
 
-    public float distanceToChase = 10;
-    public float distanceToLose = 15;
+    public float distanceToChase = 20; // Changed to 20 meters to detect player
+    public float distanceToShoot = 5; // New variable for shooting range
 
     void Start()
     {
@@ -21,6 +23,7 @@ public class Enemy : MonoBehaviour
 
         agent = GetComponent<NavMeshAgent>();
         rb = GetComponent<Rigidbody>();
+        animator = GetComponent<Animator>();
     }
 
     public void DamageEnemy() {
@@ -34,11 +37,22 @@ public class Enemy : MonoBehaviour
 
     void Chase() 
     {
+        animator.SetBool("isShooting", false);
         agent.SetDestination(targetPoint);
 
-        if (Vector3.Distance(transform.position, targetPoint) > distanceToLose) {
+        if (Vector3.Distance(transform.position, targetPoint) > distanceToChase) {
             chasing = false;
         }
+        else if (Vector3.Distance(transform.position, targetPoint) <= distanceToShoot) {
+            isShooting = true;
+        }
+    }
+
+    void Shoot() 
+    {
+        // Implement shooting logic here
+        animator.SetBool("isShooting", true);
+        Debug.Log("Shooting at player!");
     }
 
     void Update()
@@ -46,15 +60,30 @@ public class Enemy : MonoBehaviour
         targetPoint = PlayerController.instance.transform.position;
         targetPoint.y = transform.position.y;
 
-        if (!chasing) 
+        if (!isShooting) 
         {
-            if (Vector3.Distance(transform.position, targetPoint) < distanceToChase) {
-                chasing = true;
+            if (!chasing) 
+            {
+                if (Vector3.Distance(transform.position, targetPoint) < distanceToChase) {
+                    chasing = true;
+                }
+            }
+            else 
+            {
+                Chase();
             }
         }
         else 
         {
-            Chase();
+            // Stop chasing and start shooting
+            agent.isStopped = true;
+            Shoot();
+
+            // Check if the player is out of shooting range
+            if (Vector3.Distance(transform.position, targetPoint) > distanceToShoot) {
+                isShooting = false;
+                agent.isStopped = false; // Resume chasing
+            }
         }
     }
 }
